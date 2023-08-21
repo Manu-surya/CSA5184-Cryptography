b59b14b06e5158ae9c5adc9d743f84145ee952a6
Manu-surya <112241247+Manu-surya@users.noreply.github.com>
2023-08-21 09:05:26 +0530

    Add files via upload

---
+++ 1 prog.py
@@ -0,0 +1,18 @@
+def caesar_cipher(text, key):
+    encrypted_text = ""
+
+    for char in text:
+        if char.isalpha():
+            shift = ord('a') if char.islower() else ord('A')
+            encrypted_char = chr((ord(char) - shift + key) % 26 + shift)
+            encrypted_text += encrypted_char
+        else:
+            encrypted_text += char
+
+    return encrypted_text
+
+# Example usage
+plaintext =input("enter the text:")
+key = int(input("enter the key value:"))
+encrypted_text = caesar_cipher(plaintext, key)
+print("Encrypted:", encrypted_text)
